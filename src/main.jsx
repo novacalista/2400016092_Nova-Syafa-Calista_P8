af821c27e816e1e2e9287701c2980c7a951b5de9
novacalista <2400016092@webmail.uad.ac.id>
2026-01-08 13:59:05 +0700

    awokawok

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-export default function App() {
-  return (
-    <Router>
-      <div className="font-sans bg-gray-100 min-h-screen flex flex-col">
-        <Navbar />
-        
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-        
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
